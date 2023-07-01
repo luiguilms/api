db2e739d623c4ebc97bc273af225471994e77041
Luigui Lupacca <luigui.lupacca@tecsup.edu.pe>
2023-07-01 17:00:39 -0500

    api fin

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path,include
 from . import views
 from rest_framework.routers import DefaultRouter
-router=DefaultRouter()
-router.register(r'humedad',views.HumedadViewSet)
-router.register(r'planta',views.PlantaViewSet)
-router.register(r'usuario', views.UsuarioViewSet)
-urlpatterns = [
-   path('', include(router.urls))
+router = DefaultRouter()
+router.register(r'humedad', views.HumedadViewSet, basename='humedad')
+router.register(r'planta', views.PlantaViewSet, basename='planta')
+router.register(r'usuario', views.UsuarioViewSet, basename='usuario')
 
+urlpatterns = [
+    path('', include(router.urls))
 ]
--- api/views.py
+++ api/views.py
@@ -4,13 +4,37 @@ from sistema.models import Humedad, Planta,Usuario
 from .serializers import HumedadSerializer, PlantaSerializer,UsuarioSerializer
 
 class HumedadViewSet(viewsets.ModelViewSet):
-    queryset = Humedad.objects.all()
     serializer_class = HumedadSerializer
 
+    def get_queryset(self):
+        queryset = Humedad.objects.all()
+        dato = self.request.query_params.get('dato', None)
+        if dato is not None:
+            queryset = queryset.filter(dato=dato)
+        return queryset
+
 class PlantaViewSet(viewsets.ModelViewSet):
-    queryset = Planta.objects.all()
     serializer_class = PlantaSerializer
 
+    def get_queryset(self):
+        queryset = Planta.objects.all()
+        nombre = self.request.query_params.get('nombre', None)
+        if nombre is not None:
+            queryset = queryset.filter(nombre=nombre)
+        return queryset
+
 class UsuarioViewSet(viewsets.ModelViewSet):
-    queryset = Usuario.objects.all()
     serializer_class = UsuarioSerializer
+
+    def get_queryset(self):
+        queryset = Usuario.objects.all()
+        usuario = self.request.query_params.get('usuario', None)
+        clave = self.request.query_params.get('clave', None)
+
+        if usuario is not None:
+            queryset = queryset.filter(usuario=usuario)
+        if clave is not None:
+            queryset = queryset.filter(clave=clave)
+
+        return queryset
+
