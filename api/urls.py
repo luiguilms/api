d175cfc6ddb8f99620669422defff2dfa3974a74
Luigui Lupacca <luigui.lupacca@tecsup.edu.pe>
2023-06-30 19:33:47 -0500

    views y rutas actualziadas

--- api/urls.py
+++ api/urls.py
@@ -2,22 +2,10 @@ from django.urls import path,include
 from . import views
 from rest_framework.routers import DefaultRouter
 router=DefaultRouter()
-router.register(r'humedad',views.HumedadViewSet,basename='humedad')
-router.register(r'planta',views.PlantaViewSet,basename='planta')
-router.register(r'usuario', views.UsuarioViewSet,basename='usuario')
+router.register(r'humedad',views.HumedadViewSet)
+router.register(r'planta',views.PlantaViewSet)
+router.register(r'usuario', views.UsuarioViewSet)
 urlpatterns = [
-    path('',views.IndexView.as_view()),
-    path('humedad',views.HumedadView.as_view()),
-    path('humedad/<int:humedad_id>',views.HumedadDetailView.as_view()),
-    
-    path('planta',views.PlantaView.as_view()),
-    path('planta/<int:planta_id>',views.PlantaDetailView.as_view()),
-
-    
-
-    path('usuario',views.UsuarioView.as_view()),
-    path('usuario/<int:usuario_id>',views.UsuarioDetailView.as_view()),
-
-    path('admin/',include(router.urls))
+   path('', include(router.urls))
 
 ]
--- api/views.py
+++ api/views.py
@@ -1,59 +1,15 @@
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import generics, viewsets
+
+from rest_framework import  viewsets
 from sistema.models import Humedad, Planta,Usuario
 from .serializers import HumedadSerializer, PlantaSerializer,UsuarioSerializer
-
-class IndexView(APIView):
-    def get(self, request):
-        lista_humedad = Humedad.objects.all()
-        serializer_humedad = HumedadSerializer(lista_humedad, many=True)
-        return Response(serializer_humedad.data)
-
-
-class HumedadView(generics.ListCreateAPIView):
-    queryset = Humedad.objects.all()
-    serializer_class = HumedadSerializer
-
-
-class HumedadDetailView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Humedad.objects.all()
-    lookup_url_kwarg = 'humedad_id'
-    serializer_class = HumedadSerializer
-
 
 class HumedadViewSet(viewsets.ModelViewSet):
     queryset = Humedad.objects.all()
     serializer_class = HumedadSerializer
 
-
-class PlantaView(generics.ListCreateAPIView):
-    queryset = Planta.objects.all()
-    serializer_class = PlantaSerializer
-
-
-class PlantaDetailView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Planta.objects.all()
-    lookup_url_kwarg = 'planta_id'
-    serializer_class = PlantaSerializer
-
-
 class PlantaViewSet(viewsets.ModelViewSet):
     queryset = Planta.objects.all()
     serializer_class = PlantaSerializer
-
-
-
-class UsuarioView(generics.ListCreateAPIView):
-    queryset = Usuario.objects.all()
-    serializer_class = UsuarioSerializer
-
-
-class UsuarioDetailView(generics.RetrieveUpdateDestroyAPIView):
-    queryset =Usuario.objects.all()
-    lookup_url_kwarg = 'usuario_id'
-    serializer_class = UsuarioSerializer
-
 
 class UsuarioViewSet(viewsets.ModelViewSet):
     queryset = Usuario.objects.all()
